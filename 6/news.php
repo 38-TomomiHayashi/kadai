f21ec18ae0ab931e64a043ba89b56c6dd09ff6f0
38-HayashiTomomi <884tomomi@gmail.com>
2015-09-06 16:57:31 +0900

    ヘッダー・フッターを別ファイルにして読み込むように変更

---
+++ 6/news.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+    
+    <section class="news contents-box">
+        <h2 class="section-title text-center">
+            <span class="section-title__yellow">News</span>
+            <span class="section-title-ja text-center">日付</span>
+        </h2>
+        <article class="news-detail">
+            <dl class="clearfix">
+                <dd class="news-title">ニュースタイトル</dd>
+                <dd>ニュース詳細：あいうえおかきくけこさしすせそたちつてと</dd>
+            </dl>
+            
+        </article>
+    </section>
+
+    <!--#information-->
+    <?php include('footer.php'); ?>
+
+    <!--end #information-->
+<p class="btn-pageTop"><a href="#"><img src="img/btn-pagetop.png" alt=""></a></p>
+</body>
+</html>
