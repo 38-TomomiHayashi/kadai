062a9718499394f9df74eb07b9b3b730b1cfebdb
38-HayashiTomomi <884tomomi@gmail.com>
2015-09-08 01:39:03 +0900

    変更：タイトルロゴクリック時にTOPページへ遷移する

--- 6/header.php
+++ 6/header.php
@@ -1,6 +1,6 @@
     <header class="header">
         <div class="inner clearfix">
-            <h1 class="site-title"><a href="#"><img src="img/logo.png" alt="Cheese Academy Tokyo"></a></h1>
+            <h1 class="site-title"><a href="index.php"><img src="img/logo.png" alt="Cheese Academy Tokyo"></a></h1>
             <ul class="list-header text-right">
                 <li>CHEESE DEVELOPMENT</li>
                 <li>GROWTH CHEESE</li>
